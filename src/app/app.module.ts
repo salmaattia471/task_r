5dc28bbeceda342f8d60a24f21bcf46deae6db77
salmaattia471 <salma_attia@mabrains.com>
2024-07-13 23:59:39 +0300

    add home route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { TableComponent } from './components/table/table.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
